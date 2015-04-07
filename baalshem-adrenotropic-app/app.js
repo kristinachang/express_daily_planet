375b3dc3e0715d587854e65c71cf5a1e15c3e9d7
Kristina Chang <kristinachang@Kristina-Changs-MacBook-Pro.local>
2015-04-07 10:06:45 -0700

    added navigation in views

--- baalshem-adrenotropic-app/app.js
+++ baalshem-adrenotropic-app/app.js
@@ -8,6 +8,7 @@ var app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 // Refactor connection and query code
 var db = require("./models");
